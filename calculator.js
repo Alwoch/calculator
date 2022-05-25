49fa7344571e31bf78510963b841fdd5b57215c7
Alwoch <salwoch@gmail.com>
2022-05-25 18:55:12 +0300

    created first instance of the class

--- calculator.js
+++ calculator.js
@@ -1,14 +1,34 @@
-
+//display on the screen
+//select an operation
+//compute the displayed values with the selected operation
+//clear the screen
+//backspace incase of a mistake
+// Applying solid principles
+const result = document.getElementById('result');
 class Calculator {
-    //takes an operator and 2 numbers then calls the operator methods
-    constructor(number1,number2){
-        this.number1=number1;
-        this.number2=number2
+    constructor() {
+        this.data = result.textContent;
     }
-
-    populateDisplay() { }
-    operator() { }
-    updateDisplay() { }
-    clear() { }
-    backspace() { }
+    displayNumber(data) {
+        this.data += data;
+    }
+    clearData() {
+        this.data = '';
+    }
+    backSpace() {
+        this.data = result.textContent.slice(0, -1);
+    }
+    compute() {
+        try {
+            this.data = eval(result.textContent);
+        }
+        catch (error) {
+            alert('invalid');
+        }
+    }
 }
+const calc = new Calculator();
+calc.displayNumber('');
+calc.backSpace();
+calc.clearData();
+calc.compute();
--- calculator.ts
+++ calculator.ts
@@ -1,16 +1,41 @@
+//display on the screen
+//select an operation
+//compute the displayed values with the selected operation
+//clear the screen
+//backspace incase of a mistake
+ // Applying solid principles
 
-interface populateDisplay{
- num1:any;
- num2:number
-}
-class Calculator{
+const result=document.getElementById('result');
 
- //takes an operator and 2 numbers then calls the operator methods
- populateDisplay(){}
- store(){}
- operator(){}
- updateDisplay(){}
- clear(){}
- backspace(){}
- //think about the error
+
+
+class Calculator {
+ data:any=result.textContent;
+
+  displayNumber(data){
+  this.data+=data;
 }
+
+ clearData(){
+ this.data='';
+}
+
+backSpace(){
+ this.data=result.textContent.slice(0,-1);
+}
+
+compute(){
+ try {
+  this.data=eval(result.textContent);
+ } catch (error) {
+  alert('invalid');
+ }
+ 
+}
+}
+
+const calc=new Calculator();
+calc.displayNumber('');
+calc.backSpace();
+calc.clearData();
+calc.compute();
